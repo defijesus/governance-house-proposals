c784d088697cd1b7917cdbe523ffc83f3285b617
defijesus.eth <defijesus.eth>
2022-04-13 18:39:56 +0100

    add initReserve & enableBorrowingOnReserve to ILendingPoolConfigurator

--- src/interfaces/ILendingPoolConfigurator.sol
+++ src/interfaces/ILendingPoolConfigurator.sol
@@ -3,11 +3,24 @@ pragma experimental ABIEncoderV2;
 
 interface ILendingPoolConfigurator {
 
+    function initReserve(
+        address aTokenImpl,
+        address stableDebtTokenImpl,
+        address variableDebtTokenImpl,
+        uint8 underlyingAssetDecimals,
+        address interestRateStrategyAddress
+    ) external;
+
     function configureReserveAsCollateral(
         address asset,
         uint256 ltv,
         uint256 liquidationThreshold,
         uint256 liquidationBonus
+    ) external;
+
+    function enableBorrowingOnReserve(
+        address asset,
+        bool stableBorrowRateEnabled
     ) external;
 
     function setReserveFactor(address asset, uint256 reserveFactor) external;
